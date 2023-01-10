8214744534341d6b255600a40d14a38e3990768a
Pablo Mejías <116183883+Bombonas@users.noreply.github.com>
2023-01-10 18:25:43 +0100

    commit empty files

---
+++ resoruces/bbdd_provisionales.py
@@ -0,0 +1,11 @@
+
+
+players = { "11115555A":
+                { "name":"Mario" , "human":True, "bank":False, "initialCard" :"" , "priority":0, "type":40,
+                  "bet":4 ,"points":0, "cards":[], "roundPoints":0},
+            "22225555A":
+                { "name":"Pedro", "human":True, "bank":False, "initialCard":"", "priority":0, "type":40,
+                  "bet":4, "points":0, "cards":[] ,"roundPoints":0},
+            "34343434H":
+                { "name":"Bot1", "human":False, "bank":False, "initialCard":"", "priority":0, "type":30,
+                  "bet":4, "points":0, "cards":[] ,"roundPoints":0}}
