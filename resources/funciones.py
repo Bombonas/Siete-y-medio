a233a80af1a6502f84a290a0a2f0a45e53fe89f8
Pablo Mejías <116183883+Bombonas@users.noreply.github.com>
2023-01-10 19:24:00 +0100

    commit Pablo

---
+++ resources/funciones.py
@@ -0,0 +1,26 @@
+def getOpt(textOpts="",inputOptText="",rangeList=[],exceptions=[]):
+    # PRE:  Al parámetro textOpts se le pasa el string con las opciones del manú
+    #       Al parámetro inputOpt se le pasa el string con la frase que pide que escojamos una opción
+    #       El parámetro RangeList contiene las opciones contempladas por el menu
+    #       El parámetro exceptions contiene las posibles excepciones que pueden generarse
+    # POST: Devolverá un valor de RangeList si la selección es correcta y devolverá un valor de exceptions si ha ocurrido
+    #       un error
+    correct = False
+    opc = ''
+    while not correct:
+        print(textOpts)
+        opc = input(inputOptText)
+        try:
+            opc = int(opc)
+            if opc not in rangeList and opc not in exceptions:
+                raise TypeError('Incorrect Option')
+            else:
+                correct = True
+        except ValueError:
+            print('Please, introduce only numbers')
+            input("Enter to continue")
+        except TypeError as e:
+            print(e)
+            input("Enter to continue")
+    return opc
+
