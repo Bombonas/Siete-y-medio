a7ed37d208dc2c847f04a99ca0e681c6dc69f7e9
Pablo Mejías <116183883+Bombonas@users.noreply.github.com>
2023-01-10 18:30:23 +0100

    commit empty files

--- resoruces/bbdd_provisionales.py
+++ resoruces/bbdd_provisionales.py
@@ -1,11 +1,15 @@
 
 
 players = { "11115555A":
-                { "name":"Mario" , "human":True, "bank":False, "initialCard" :"" , "priority":0, "type":40,
-                  "bet":4 ,"points":0, "cards":[], "roundPoints":0},
+                { "name":"Mario", "human":True, "bank":False, "initialCard":"", "priority":0, "type":40,
+                  "bet":4, "points":0, "cards":[], "roundPoints":0},
             "22225555A":
                 { "name":"Pedro", "human":True, "bank":False, "initialCard":"", "priority":0, "type":40,
-                  "bet":4, "points":0, "cards":[] ,"roundPoints":0},
+                  "bet":4, "points":0, "cards":[], "roundPoints":0},
             "34343434H":
                 { "name":"Bot1", "human":False, "bank":False, "initialCard":"", "priority":0, "type":30,
-                  "bet":4, "points":0, "cards":[] ,"roundPoints":0}}
+                  "bet":4, "points":0, "cards":[], "roundPoints":0}
+            "11111111A":
+                { "name":"Bot2", "human":False, "bank":False, "initialCard":"", "priority":0, "type":50,
+                  "bet":4, "points":0, "cards":[], "roundPoints":0}
+            }
