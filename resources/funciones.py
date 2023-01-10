5a13a9ae8eb06750df18a486b00d6416e12b3fd7
Arguelless <marguelles@protonmail.com>
2023-01-10 19:23:10 +0100

    Commit

--- resources/funciones.py
+++ resources/funciones.py
@@ -1,10 +1,17 @@
+import datetime
 from os import *
 from resources.prints import *
 from resources.bbdd_provisionales import *
-
+import random
 
 def clear():
     if name == 'nt':
         system('cls')
     else:
         system('clear')
+
+game = []
+def setGamePriority(mazo=[], jugadores=[]):
+
+
+def resetPoints():
