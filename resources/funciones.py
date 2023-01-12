8deb8bb2ca7e2032d320d6ef2c038ebed97504a0
Arguelless <marguelles@protonmail.com>
2023-01-12 18:34:01 +0100

    newcommit

--- resources/funciones.py
+++ resources/funciones.py
@@ -118,3 +118,187 @@ def setBets():
 
 def standardRound(id, mazo=[]):
     pass
+
+def getOpt(textOpts="",inputOptText="",rangeList=[],exceptions=[]):
+    # PRE:  Al parámetro textOpts se le pasa el string con las opciones del manú
+    #       Al parámetro inputOpt se le pasa el string con la frase que pide que escojamos una opción
+    #       El parámetro RangeList contiene las opciones contempladas por el menu
+    #       El parámetro exceptions contiene las posibles excepciones que pueden generarse
+    # POST: Devolverá un valor de RangeList si la selección es correcta y devolverá un valor de exceptions si ha ocurrido
+    #       un error
+    correct = False
+    opc = ''
+    while not correct:
+        print(textOpts)
+        opc = input(inputOptText)
+        try:
+            opc = int(opc)
+            if opc not in rangeList and opc not in exceptions:
+                raise TypeError('Incorrect Option')
+            else:
+                correct = True
+        except ValueError:
+            print('Please, introduce only numbers')
+            input("Enter to continue")
+        except TypeError as e:
+            print(e)
+            input("Enter to continue")
+    return opc
+
+def orderPlayersByPoints(listaDNIs):
+    dic_PL_Points = {}
+    # Llamamos a una funcion que pida los DNIs y calcule los puntos que tiene un jugador a la BBDD,
+    # y los devuelva en formato diccionario; dni : puntos
+
+    for pasada in range(listaDNIs - 1):
+        lista_ordenada = True
+        for i in range(len(listaDNIs) - 1 - pasada):
+            if dic_PL_Points[listaDNIs[i]] < dic_PL_Points[listaDNIs[i + 1]]:
+                lista_ordenada = False
+                aux = listaDNIs[i]
+                listaDNIs[i] = listaDNIs[i + 1]
+                listaDNIs[i + 1] = aux
+        if lista_ordenada:
+            break
+    return listaDNIs
+
+def chanceExceedingSevenAndHalf(id, mazo):
+    bad_cards = 0
+    for i in mazo:
+        if cartas[i]["realValue"] + players[id]["roundPoints"] > 7.5:
+            bad_cards += 1
+
+    return (bad_cards * 100) / len(mazo)
+
+def printPlayerStats(id):
+    print("Stats of {}".format(players[id]["name"]).center(140, "*"))
+    for i in players[id]:
+        if i == "cards":
+            print(str(i).ljust(55), end="")
+            primero = True
+            for j in players[id]["cards"]:
+                if primero:
+                    primero = False
+                    print(str(j), sep="", end="")
+                else:
+                    print(";", str(j), sep="", end="")
+            print()
+        else:
+            print(str(i).ljust(55), str(players[id][i]).ljust(4), sep="")
+
+def baknOrderNewCard(id, game):
+        earnings = 0
+        looses = 0
+        ret = True
+        for i in game:
+            if i != id:
+                if(players[i]["roundPoints"] <= 7.5 and players[i]["roundPoints"] <= players[id]["roundPoints"]) or players[i]["roundPoints"] > 7.5:
+                    earnings += players[i]["bet"]
+                else:
+                    if players[i]["roundPoints"] == 7.5:
+                        looses += players[i]["bet"] * 2
+                    else:
+                        looses += players[i]["bet"]
+        if earnings > looses:
+            ret = False
+
+        return ret
+
+def nif_validator():
+    # PRE:
+    # POST: Devuelve un NIF válido
+    correct = False
+    newnif = ''
+    while not correct:
+        try:
+            newnif = input('Introduce the NIF: ')
+            if not len(newnif) == 9:
+                raise ValueError('Invalid NIF length')
+            elif not newnif[:8].isdigit():
+                raise ValueError('Invalid NIF numbers')
+            elif not newnif[8].isalpha():
+                raise ValueError('Invalid NIF letter')
+            elif not letrasDni[int(newnif[:8]) % 23].casefold() == newnif[8].casefold():
+                raise ValueError('Incorrect NIF letter')
+            elif newnif.upper() in players:
+                raise ValueError('That NIF already exists in the database')
+            correct = True
+        except ValueError as e:
+            print(e)
+
+    return newnif.upper()
+
+def addRemovePlayers():
+    menu = "1)New Human Player\n2)New Bot\n3)Show/Remove Players\n4)Go back"
+    opt = getOpt(menu, "Option: ", [1, 2, 3, 4])
+    if opt == 1:
+        print("opt1")
+    elif opt == 2:
+        print("opt2")
+    elif opt == 3:
+        print("opt3")
+    elif opt == 4:
+        print("opt4")
+
+def setMaxRounds():
+    correct = False
+    rounds = 5
+    while not correct:
+        rounds = input("Max Rounds : ")
+        if not rounds.isdigit():
+            print("Please, only introduce numbers")
+        elif int(rounds) <= 0:
+            print("Please, introduce a number bigger than 0")
+        else:
+            correct = True
+    contextGame["maxRounds"] = rounds
+
+def settings():
+    menu = "1)Set Game\n2)Set Card's Deck\n3)Set Max Rounds(Default 5 Rounds)\n4)Go back"
+    opt = getOpt(menu, "Option: ", [1, 2, 3, 4])
+    if opt == 1:
+        print("opt1")
+    elif opt == 2:
+        print("opt2")
+    elif opt == 3:
+        setMaxRounds()
+    elif opt == 4:
+        print("opt4")
+
+def newRandomDNI():
+    DNI = random.randint(10000000, 99999999)
+    letra = letrasDni[DNI % 23]
+    DNI = str(DNI) + letra.upper()
+    return DNI
+
+def setNewPlayer(human=True):
+    dni = ""
+    profile = 0
+    name = ""
+    if human:
+        dni = nif_validator()
+    else:
+        dni = newRandomDNI()
+
+    opt = getOpt("Select your Profile:\n1)Cautious\n2)Moderated\n3)Bold", "Option", [1, 2, 3])
+    if opt == 1:
+        profile = 30
+    elif opt == 2:
+        profile = 40
+    else:
+        profile = 50
+
+    correct = False
+    while not correct:
+        name = input("Name: ")
+        if not name.isalnum():
+            print("Incorrect name, please, enter a name not empty with only letters")
+        else:
+            correct = True
+    tup_player = newPlayer(dni, name, profile, human)
+
+
+def newPlayer(dni, name, profile, human):
+    dic_aux = {"name": name, "human": human, "bank": False, "initialCard": "", "priority": 0, "type": profile,
+               "bet": 4, "points": 0, "cards": [], "roundPoints": 0}
+    return (dni, dic_aux)
