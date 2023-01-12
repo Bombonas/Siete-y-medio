d908ab09d68c7150a70d4308499c5881de45102c
Pablo Mejías <116183883+Bombonas@users.noreply.github.com>
2023-01-12 18:35:10 +0100

    commit backup

--- resources/bbdd_provisionales.py
+++ resources/bbdd_provisionales.py
@@ -76,7 +76,6 @@ player_game_round = {'round': {
 
 cardgame_ids = []
 
-
 letrasDni = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N",
              "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
 
--- resources/funciones.py
+++ resources/funciones.py
@@ -1,120 +1,186 @@
-import datetime
-from os import *
-from resources.prints import *
 from resources.bbdd_provisionales import *
+from resources.prints import *
 import random
-import math
+def getOpt(textOpts="",inputOptText="",rangeList=[],exceptions=[]):
+    # PRE:  Al parámetro textOpts se le pasa el string con las opciones del manú
+    #       Al parámetro inputOpt se le pasa el string con la frase que pide que escojamos una opción
+    #       El parámetro RangeList contiene las opciones contempladas por el menu
+    #       El parámetro exceptions contiene las posibles excepciones que pueden generarse
+    # POST: Devolverá un valor de RangeList si la selección es correcta y devolverá un valor de exceptions si ha ocurrido
+    #       un error
+    correct = False
+    opc = ''
+    while not correct:
+        print(textOpts)
+        opc = input(inputOptText)
+        try:
+            opc = int(opc)
+            if opc not in rangeList and opc not in exceptions:
+                raise TypeError('Incorrect Option')
+            else:
+                correct = True
+        except ValueError:
+            print('Please, introduce only numbers')
+            input("Enter to continue")
+        except TypeError as e:
+            print(e)
+            input("Enter to continue")
+    return opc
 
-def clear():
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+def orderPlayersByPoints(listaDNIs):
+    dic_PL_Points = {}
+    # Llamamos a una funcion que pida los DNIs y calcule los puntos que tiene un jugador a la BBDD,
+    # y los devuelva en formato diccionario; dni : puntos
 
+    for pasada in range(listaDNIs - 1):
+        lista_ordenada = True
+        for i in range(len(listaDNIs) - 1 - pasada):
+            if dic_PL_Points[listaDNIs[i]] < dic_PL_Points[listaDNIs[i + 1]]:
+                lista_ordenada = False
+                aux = listaDNIs[i]
+                listaDNIs[i] = listaDNIs[i + 1]
+                listaDNIs[i + 1] = aux
+        if lista_ordenada:
+            break
+    return listaDNIs
 
-game = list(players)
+def chanceExceedingSevenAndHalf(id, mazo):
+    bad_cards = 0
+    for i in mazo:
+        if cartas[i]["realValue"] + players[id]["roundPoints"] > 7.5:
+            bad_cards += 1
 
+    return (bad_cards * 100) / len(mazo)
 
-def setGamePriority(mazo=[], jugadores=[]):
-    cartasRepartidas = {}
-    cartasOrdenadas = {}
-    for i in range(len(jugadores)):
-        cartaRandom = random.choice(mazo)
-        cartasRepartidas[jugadores[i]] = cartaRandom
-        mazo.remove(cartaRandom)
-    mida_llista = len(jugadores)
-    llista = []
-    for i in cartasRepartidas:
-        carta = cartasRepartidas[i]
-        llista.append(cartas[carta]['value'])
-    for i in range(mida_llista - 1):
-        for j in range(0, mida_llista - i - 1):
-            if llista[j] < llista[j + 1]:
-                llista[j], llista[j + 1] = llista[j + 1], llista[j]
-                jugadores[j], jugadores[j + 1] = jugadores[j + 1], jugadores[j]
-            elif llista[j] == llista[j + 1] and cartas[cartasRepartidas[jugadores[j]]]['priority'] < \
-                    cartas[cartasRepartidas[jugadores[j + 1]]]['priority']:
-                jugadores[j], jugadores[j + 1] = jugadores[j + 1], jugadores[j]
-
-    for i in jugadores:
-        cartasOrdenadas.update({i: cartasRepartidas[i]})
-
-    return cartasOrdenadas
-
-
-def resetPoints():
-    for i in game:
-        players[i]['points'] = 20
-
-
-def generate_game_id():
-    new_cardgame_id = len(cardgame_ids)
-    return new_cardgame_id
-
-def card_id_list(diccionario):
-    lista_card_id = []
-    for i in diccionario:
-        lista_card_id.append(diccionario[i])
-
-    return lista_card_id
-
-
-def fill_player_game(gameID, jugadores=[], card_id_list=[], starting_points_list=[], ending_points_list=[]):
-    player_game.update({gameID:''})
-    for i in range(len(jugadores)):
-        if i == 0:
-            player_game[gameID] = {jugadores[i]: {'initial_card_id': card_id_list[i], 'starting_points':
-                starting_points_list[i], 'ending_points': ending_points_list[i]}}
+def printPlayerStats(id):
+    print("Stats of {}".format(players[id]["name"]).center(140, "*"))
+    for i in players[id]:
+        if i == "cards":
+            print(str(i).ljust(55), end="")
+            primero = True
+            for j in players[id]["cards"]:
+                if primero:
+                    primero = False
+                    print(str(j), sep="", end="")
+                else:
+                    print(";", str(j), sep="", end="")
+            print()
         else:
-            player_game[gameID][jugadores[i]] = {'initial_card_id': card_id_list[i], 'starting_points':
-                starting_points_list[i], 'ending_points': ending_points_list[i]}
-    return player_game
-# fill_player_game(generate_game_id(), list(setGamePriority(list(cartas), list(players))), card_id_list(setGamePriority(list(cartas), list(players))),
-#                  [20,20,20,20], [3, 1, 45, 0])
+            print(str(i).ljust(55), str(players[id][i]).ljust(4), sep="")
 
-def checkMinimun2PlayerWithPoints():
-    #Funcion que devuelve True si hay 2 o más jugadores con puntos, de lo contrario devuelve False
+def baknOrderNewCard(id, game):
+        earnings = 0
+        looses = 0
+        ret = True
+        for i in game:
+            if i != id:
+                if(players[i]["roundPoints"] <= 7.5 and players[i]["roundPoints"] <= players[id]["roundPoints"]) or players[i]["roundPoints"] > 7.5:
+                    earnings += players[i]["bet"]
+                else:
+                    if players[i]["roundPoints"] == 7.5:
+                        looses += players[i]["bet"] * 2
+                    else:
+                        looses += players[i]["bet"]
+        if earnings > looses:
+            ret = False
 
-    contador = 0
-    for i in game:
-        if players[i]['points'] > 0:
-            contador += 1
+        return ret
 
-    if contador < 2:
-        seguir_jugando = False
+def nif_validator():
+    # PRE:
+    # POST: Devuelve un NIF válido
+    correct = False
+    newnif = ''
+    while not correct:
+        try:
+            newnif = input('Introduce the NIF: ')
+            if not len(newnif) == 9:
+                raise ValueError('Invalid NIF length')
+            elif not newnif[:8].isdigit():
+                raise ValueError('Invalid NIF numbers')
+            elif not newnif[8].isalpha():
+                raise ValueError('Invalid NIF letter')
+            elif not letrasDni[int(newnif[:8]) % 23].casefold() == newnif[8].casefold():
+                raise ValueError('Incorrect NIF letter')
+            elif newnif.upper() in players:
+                raise ValueError('That NIF already exists in the database')
+            correct = True
+        except ValueError as e:
+            print(e)
+
+    return newnif.upper()
+
+def addRemovePlayers():
+    menu = "1)New Human Player\n2)New Bot\n3)Show/Remove Players\n4)Go back"
+    opt = getOpt(menu, "Option: ", [1, 2, 3, 4])
+    if opt == 1:
+        print("opt1")
+    elif opt == 2:
+        print("opt2")
+    elif opt == 3:
+        print("opt3")
+    elif opt == 4:
+        print("opt4")
+
+def setMaxRounds():
+    correct = False
+    rounds = 5
+    while not correct:
+        rounds = input("Max Rounds : ")
+        if not rounds.isdigit():
+            print("Please, only introduce numbers")
+        elif int(rounds) <= 0:
+            print("Please, introduce a number bigger than 0")
+        else:
+            correct = True
+    contextGame["maxRounds"] = rounds
+
+def settings():
+    menu = "1)Set Game\n2)Set Card's Deck\n3)Set Max Rounds(Default 5 Rounds)\n4)Go back"
+    opt = getOpt(menu, "Option: ", [1, 2, 3, 4])
+    if opt == 1:
+        print("opt1")
+    elif opt == 2:
+        print("opt2")
+    elif opt == 3:
+        setMaxRounds()
+    elif opt == 4:
+        print("opt4")
+
+def newRandomDNI():
+    DNI = random.randint(10000000, 99999999)
+    letra = letrasDni[DNI % 23]
+    DNI = str(DNI) + letra.upper()
+    return DNI
+
+def setNewPlayer(human=True):
+    dni = ""
+    profile = 0
+    name = ""
+    if human:
+        dni = nif_validator()
     else:
-        seguir_jugando = True
+        dni = newRandomDNI()
 
-    return seguir_jugando
+    opt = getOpt("Select your Profile:\n1)Cautious\n2)Moderated\n3)Bold", "Option", [1, 2, 3])
+    if opt == 1:
+        profile = 30
+    elif opt == 2:
+        profile = 40
+    else:
+        profile = 50
 
-def orderAllPlayers():
-    #Funcion que crea una lista con los puntos de los jugadores y ordena la lista de jugadores de forma inversa segun sus puntos, pone la banca al principio
-    #POST: Devuelve una lista con los ID_player ordenados.
-    lista_puntos = []
-    for i in game:
-        lista_puntos.append(players[i]['points'])
+    correct = False
+    while not correct:
+        name = input("Name: ")
+        if not name.isalnum():
+            print("Incorrect name, please, enter a name not empty with only letters")
+        else:
+            correct = True
+    tup_player = newPlayer(dni, name, profile, human)
 
 
-    mida_llista = len(lista_puntos)
-
-    for i in range(mida_llista - 1):
-        for j in range(0, mida_llista - i - 1):
-            if lista_puntos[j] > lista_puntos[j + 1]:
-                lista_puntos[j], lista_puntos[j + 1] = lista_puntos[j + 1], lista_puntos[j]
-                game[j], game[j + 1] = game[j + 1], game[j]
-
-    for i in game:
-        if players[i]['bank'] == True:
-            game.remove(i)
-            game.append(i)
-
-    return game
-print(orderAllPlayers())
-def setBets():
-    #Funcion que establece las apuestas según el tipo de jugador
-    for i in game:
-        if players[i]['points'] > 0:
-            players[i]['bet'] = math.ceil(players[i]['points'] / 100 * players[i]['type'])
-
-def standardRound(id, mazo=[]):
-    pass
+def newPlayer(dni, name, profile, human):
+    dic_aux = {"name": name, "human": human, "bank": False, "initialCard": "", "priority": 0, "type": profile,
+               "bet": 4, "points": 0, "cards": [], "roundPoints": 0}
+    return (dni, dic_aux)
